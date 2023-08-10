7b5bcc83ea422981b4d3029b0f6d065b40abec4c
Jorge Leandro <js.leandro26@gmail.com>
2023-08-10 17:50:37 -0300

    fix: detalhamento de testes

--- projeto/department/tests.py
+++ projeto/department/tests.py
@@ -2,27 +2,26 @@ from django.test import TestCase
 from rest_framework.test import APIClient
 from rest_framework import status
 from .models import Department
-from . import serializers
-from rest_framework import serializers
 class DepartamentApiTests(TestCase):
 
     def setUp(self):
         self.client = APIClient()
+        self.department = Department.objects.create(descricao='asas')
 
     def test_create_object(self):
         data = {'descricao': 'asas'}
-        response = self.client.post('/department/criar', data, format='json')
+        response = self.client.post('/department/', data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_list_object(self):
-        response = self.client.get('/department', follow=True)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_delete_object(self):
-        response = self.client.delete('/department/delete/23')
+        response = self.client.get('/department/', follow=True)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_edit_object(self):
         data = {'descricao': '1122'}
-        response = self.client.put('/department/editar/23', data, format='json')
+        response = self.client.put(f'/department/{self.department.id}', data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_delete_object(self):
+        response = self.client.delete(f'/department/{self.department.id}')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
