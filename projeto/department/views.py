57d34ea6eeec51a3df6e183a339538f60b3e1114
Jorge Leandro <js.leandro26@gmail.com>
2023-08-10 17:15:38 -0300

    refactor: generics do django

--- projeto/department/views.py
+++ projeto/department/views.py
@@ -1,67 +1,12 @@
-from rest_framework.generics import GenericAPIView, ListAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
-from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
 
 from . import serializers
 from .models import Department
 
-class CreateDepartamentoView(CreateAPIView):
-
+class ListCreateDepartamentoView(ListCreateAPIView):
+    queryset = Department.objects.all()
     serializer_class = serializers.DepartamentoCreateSerializer
 
-    def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        data = serializer.data
-
-        return Response(data, status=status.HTTP_201_CREATED)
-    
-class ListarDepartamentoView(ListAPIView):
-
+class DetailApiView(RetrieveUpdateDestroyAPIView):
+    queryset = Department.objects.all()
     serializer_class = serializers.DepartamentoSerializer
-
-    def get(self, request):
-        queryset = Department.objects.all()
-        serializer = self.get_serializer(queryset, many=True)
-        data = serializer.data
-        return Response(data, status=status.HTTP_200_OK)
-    
-    
-class ListarOneDepartamentoView(ListAPIView):
-
-    serializer_class = serializers.DepartamentoSerializer
-
-    def get(self, request, id):
-        queryset = Department.objects.filter(pk=id)
-        serializer = self.get_serializer(queryset, many=True)
-        data = serializer.data
-        return Response(data, status=status.HTTP_200_OK)
-    
-class EditDepartamentoView(UpdateAPIView):
-
-    serializer_class = serializers.DepartamentoEditSerializer
-
-    def put(self, request, id):
-
-        descricao = request.data.get('descricao')
-        queryset = Department.objects.filter(pk=id).update(descricao=descricao)
-        self.get_serializer(queryset, many=True)
-
-        getUsers = Department.objects.filter(pk=id)
-        serializerTwo = self.get_serializer(getUsers, many=True)
-        data = serializerTwo.data
-        return Response(data, status=status.HTTP_200_OK)
-    
-    
-class DeleteDepartamentoView(DestroyAPIView):
-
-    serializer_class = serializers.DepartamentoDeleteSerializer
-
-    def delete(self, request, id):
-        queryset = Department.objects.filter(pk=id).delete()
-        self.get_serializer(queryset, many=True)
-        return Response({'ok'}, status=status.HTTP_200_OK)
-
-# Create your views here.
